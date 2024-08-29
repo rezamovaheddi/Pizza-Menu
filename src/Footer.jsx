249df2ef4282bd7eecc1849535f539e32d884db4
rezamovaheddi <rmwhdy8@gamil.com>
2024-08-29 19:50:25 +0330

    add if else for time anf open

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,15 +4,13 @@ export default function(){
     const hour = new Date().getHours();
     const openOur = 7;
     const closeOur = 22;
-    let chk = hour >= openOur && hour <= closeOur;
-
     return (
         <>
         <footer className='footer'>
             <div className='order'>
                 {hour >= openOur && hour <= closeOur
-                ? <p style={{color:'green'}}>We are open now</p>
-                : <p style={{color:'red'}}>We are close now</p>}
+                ? <p style={{color:'green'}}>We are open now to {closeOur}:00 pm</p>
+                : <p style={{color:'red'}}>We are close now We open at {closeOur}:00 am</p>}
                 <span>{new Date().toLocaleTimeString()}</span>
                 <button className='btn'>ORDER</button>
             </div>

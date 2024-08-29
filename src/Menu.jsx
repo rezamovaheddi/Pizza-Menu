2f48dd2ee37de00d9423128adb956d5a5a87138c
rezamovaheddi <rmwhdy8@gamil.com>
2024-08-29 19:51:10 +0330

    add list and connect for items

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,17 +4,19 @@ import { pizzaData } from "./data";
 
 
   function Menu(){
+
+    const Pizzas =pizzaData;
+
       return (
           <main className="menu">
             <h2>Our Menu</h2>
+
+            {Pizzas.length < 1 
+            ? <h1 style={{color: 'red'}}>We aren`t item</h1>:
             <ul className="pizzas">
-            {pizzaData.map(m=>
-            <Pizza 
-             pizzaO={m}
-            key={m.name}
-             />
-            )}
+            {Pizzas.map(m=><Pizza pizzaO={m} key={m} />)}
             </ul>
+            }
           </main>
       )
       

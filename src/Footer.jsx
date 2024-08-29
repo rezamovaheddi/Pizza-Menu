fb8c8010a8653b754c22d02831aeb5e2fe2ae025
rezamovaheddi <rmwhdy8@gamil.com>
2024-08-29 19:32:20 +0330

    build a btn and style

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,15 +4,18 @@ export default function(){
     const hour = new Date().getHours();
     const openOur = 7;
     const closeOur = 22;
+    let chk = hour >= openOur && hour <= closeOur;
 
     return (
         <>
         <footer className='footer'>
-
-            {hour >= openOur && hour <= closeOur
-            ? <p style={{color:'green'}}>We are open now</p>
-            : <p style={{color:'red'}}>We are close now</p>}
-        <span>{new Date().toLocaleTimeString()}</span>
+            <div className='order'>
+                {hour >= openOur && hour <= closeOur
+                ? <p style={{color:'green'}}>We are open now</p>
+                : <p style={{color:'red'}}>We are close now</p>}
+                <span>{new Date().toLocaleTimeString()}</span>
+                <button className='btn'>ORDER</button>
+            </div>
         </footer>
 
         </>
